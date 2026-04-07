def5e0516d2b19dd9811eb08c56648b7ea9d9c13
chiendong1048-bit <chiendong1048@gmail.com>
2026-04-07 10:14:12 +0700

    Hoan thanh bai tap hinh hoc

---
+++ bai_tap_1.py
@@ -0,0 +1,10 @@
+# Khai báo giá trị biến
+chieu_dai = 10
+chieu_rong = 5
+#tính toán
+chu_vi = (chieu_dai + chieu_rong) * 2
+dien_tich = (chieu_dai * chieu_rong)
+print("Chieu dai hinh chu nhat la:", chieu_dai)
+print("Chieu rong hinh chu nhat la:", chieu_rong)
+print("Dien tich hinh chu nhat la:", dien_tich)
+print("Chu vi hinh chu nhat la:", chu_vi)
