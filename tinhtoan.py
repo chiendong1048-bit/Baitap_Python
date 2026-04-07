b4539025dc7c2ac8253cba1481ee55bc925ee338
chiendong1048-bit <chiendong1048@gmail.com>
2026-04-07 10:00:54 +0700

    Bai tap tinh toan dau tien

---
+++ tinhtoan.py
@@ -0,0 +1,17 @@
+#Đây là bài tập tính toán đầu tiên
+a = 10
+b = 5
+print("Tổng của a và b là:", a + b)
+print("Tích của a và b là:", a*b)
+name = "Chien"
+age = 19 
+gpa_goal = 9.5
+
+a = 10
+b = 3
+print(a + b)
+print(a * b)
+print (a ** b)
+print(a % b)
+print(a / b)
+print(a - b)
